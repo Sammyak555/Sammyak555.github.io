8d9c6031a7690b995905021cd0af3eb459c69d87
Sammyak555 <sammyak.deosale.1@gmail.com>
2022-12-18 16:48:46 +0530

    ch

--- src/App.js
+++ src/App.js
@@ -3,27 +3,34 @@ import './Styles/global.css';
 import Navbar from './Components/Navbar';
 import Header from './Components/Header';
 import  Skills  from './Pages/Skills';
-import { AllRoutes } from './Components/AllRoutes';
-import { About } from './Pages/About';
 import { Projects } from './Pages/Projects';
 import Contact from './Pages/Contact';
 import Calendor from './Pages/GitCalender';
 import Statistics from './Pages/Statistics';
 
+import { About } from './Pages/About';
+import { useEffect, useRef, useState } from 'react';
+
 function App() {
+  
+  
   return (
     <div className="App">
       <Navbar />
-      <Header/>
-      <About/>
-      <Projects/>
-     <div>
-     <a id='skills'></a>
+      <div >
+      <Header/></div>
+      <div className='boxes'>
+      <About/></div>
+      <div >
+      <Projects/></div>
+     <div className='boxes'>
      <Skills/>
      </div>
      {/* <Calendor/> */}
-     <Statistics />
-     <Contact/>
+     <div className='boxes'>
+     <Statistics /></div>
+     <div className='boxes'>
+     <Contact/></div>
     </div>
   );
 }
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,27 +1,31 @@
-import "../css/About.css"
+
+import { useInView } from "react-intersection-observer"
+import  "../css/About.css"
 // import background from "../download/background.mov"
 
-export const About=()=>{
+export const About = () => {
+  const { ref:myRef, inView:elvisible} = useInView();
+  const { ref:boxRef, inView:boxvisible} = useInView();
+  const { ref:coderRef, inView:codervisible} = useInView();
  
-    return(
-        <div >
-            <div className="aboutebox" id='aboutme'>
-      {/* <video   className="aboutevideo" src={background} autoPlay loop muted/> */}
-      <div className="fake">
-        {/* <img src="https://www.techbabble.zone/content/images/2021/07/46207-programmer-1.gif" alt="" /> */}
-      </div>
-      {/* <div className="boximg">
-      <img src="https://www.techbabble.zone/content/images/2021/07/46207-programmer-1.gif" alt="" />
-      </div> */}
+  
+  return (
+    <div >
+      <div  className="aboutebox" id='aboutme' ref={myRef} >
       <h1>About Me</h1>
-    <div className='box' >
-     
-      <p className='disc'>
-      My name is Sammyak. I'm a full-stack web-developer based in Masai School, Benguluru. I am a B.Tech graduate and love exploring new technologies in the field of Web Development and always try to adapt to it. I believe in the concept of continuous learning by regularly upgrading my skills and enhancing my knowledge. I have learned MERN stack, data structures, algorithms, and soft skills at Masai School. And besides coding, I love traveling, singing and cooking As a developer, my hunger for learning has drastically increased and I want to centralize my skills and learning for enhancement of an organization and more of it, to enhance my career.
-      </p>
-    </div>
-    </div>
+        {/* <video   className="aboutevideo" src={background} autoPlay loop muted/> */}
+       <div className="abtcontent hidden" >
+       <div ref={coderRef} className={` ${''} ${codervisible?"coderanimation":''}` }>
+          <img src="https://www.techbabble.zone/content/images/2021/07/46207-programmer-1.gif" alt="" />
         </div>
-    );
-    
+        <div className={`${"box"} ${boxvisible ? "animatebox":""}`}  ref={boxRef}>
+          <p className='disc' data-aos='zoom-in' data-aos-duration='3000'>
+            My name is Sammyak. I'm a full-stack web-developer based in Masai School, Benguluru. I am a B.Tech graduate and love exploring new technologies in the field of Web Development and always try to adapt to it. I believe in the concept of continuous learning by regularly upgrading my skills and enhancing my knowledge. I have learned MERN stack, data structures, algorithms, and soft skills at Masai School. And besides coding, I love traveling, singing and cooking As a developer, my hunger for learning has drastically increased and I want to centralize my skills and learning for enhancement of an organization and more of it, to enhance my career.
+          </p>
+        </div>
+       </div>
+      </div>
+    </div>
+  );
+
 }
