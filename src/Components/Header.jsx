a8fe06cea24f5967bed3f116e57a637a8f3b505f
Sammyak555 <sammyak.deosale.1@gmail.com>
2022-12-31 12:11:56 +0530

    abt

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
-import { background, Button, Icon, Image } from "@chakra-ui/react";
-import React, { useState } from "react";
+import { Button, Icon, Image } from "@chakra-ui/react";
+import React from "react";
 import '../Styles/global.css';
 import Typewriter from "typewriter-effect"
 import  {RiFileUserLine}  from 'react-icons/ri'
@@ -7,26 +7,25 @@ import myimage from "../Styles/myimage.png"
 import Sammyak_MERN_Resume1 from "../Resume/Sammyak_Deosale_Mern_Developer_Resume.pdf"
 
 export default function Header() {
-        const [state]=useState({
-          title:"Hellow",
-          titletwo:"I'm",
-          titlethree:"Full Stack Web Developer",
+    //     const [state]=useState({
+    //       title:"Hellow",
+    //       titletwo:"I'm",
+    //       titlethree:"Full Stack Web Developer",
         
-      })
+    //   })
     return (
         <header>
             {/* <NavBar /> */}
             <div className="header flex light-bg">
                 <div className="content">
                     <h1 className="white">Hi 👋,</h1>
-                       <h1 className="white fontsty" style={{fontSize:"30px"}}>I am Sammyak Deosale <span className="typewr" style={{color:"yellow"}}><Typewriter  options={{
+                       <h1 className="white fontsty" style={{fontSize:"30px"}}>I am Sammyak Deosale <span className="typewr" style={{color:"rgb(69, 124, 178)"}}><Typewriter  options={{
                              autoStart:true,
                             loop:true,
-                            delay:40,
+                            delay:50,
                             strings:[
                             "Full Stack Web Developer"]
                                 }}/></span> </h1>
-                       <h1></h1>
                     <br />
                     
                     <h4 className="gray" style={{fontSize:"25   px"}}>I design and code beautiful things, and I love what I do.</h4>
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -22,9 +22,9 @@ export const About = () => {
           <img src="https://www.techbabble.zone/content/images/2021/07/46207-programmer-1.gif" alt="" />
         </div>
         <div className={"box"} >
-          <p className='disc' data-aos='zoom-in' data-aos-duration='3000'>
-            My name is Sammyak. I'm a full-stack web-developer based in Masai School, Benguluru. I am a B.Tech graduate and love exploring new technologies in the field of Web Development and always try to adapt to it. I believe in the concept of continuous learning by regularly upgrading my skills and enhancing my knowledge. I have learned MERN stack, data structures, algorithms, and soft skills at Masai School. And besides coding, I love traveling, singing and cooking As a developer, my hunger for learning has drastically increased and I want to centralize my skills and learning for enhancement of an organization and more of it, to enhance my career.
-          </p>
+          <p className='disc' data-aos='fade-left' data-aos-duration='1500'>
+          Full-Stack Web Developer with the ability to learn and collaborate in rapidly changing environments and compositions. Worked through 1200+ hours of Bootcamp structure learning JavaScript, Node.Js, React, Redux, MongoDB, Express, HTML5, and CSS3.
+            </p>
         </div>
        </div>
       </div>
