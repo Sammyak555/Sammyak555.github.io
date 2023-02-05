aac95b7d7a712ccd748366b651719f2593108abd
Sammyak555 <sammyak.deosale.1@gmail.com>
2023-02-05 17:46:37 +0530

    resume

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,24 @@ import '../Styles/global.css';
 import Typewriter from "typewriter-effect"
 import  {RiFileUserLine}  from 'react-icons/ri'
 import myimage from "../Styles/myimage.png"
-import Sammyak_MERN_Resume1 from "../Resume/Sammyak_Deosale_Resume.pdf"
+import Sammyak_MERN_Resume1 from "../Resume/fw19_1004_sammyak_deosale_Resume.pdf"
 import { SiLinkedin } from "react-icons/si";
 
 export default function Header() {
+    const onButtonClick = () => {
+        // using Java Script method to get PDF file
+        fetch(Sammyak_MERN_Resume1).then(response => {
+            response.blob().then(blob => {
+                // Creating new object of PDF file
+                const fileURL = window.URL.createObjectURL(blob);
+                // Setting various property values
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = Sammyak_MERN_Resume1;
+                alink.click();
+            })
+        })
+    }
     return (
         <header>
             {/* <NavBar /> */}
@@ -24,7 +38,7 @@ export default function Header() {
                     
                     <h4 className="gray fontsty" style={{fontSize:"25   px"}}>I would love to practice and use my skills to help an organisation uplift by truly folllowing the phrase "Rise by lifting others".</h4>
                     <br />
-                    <a href='https://drive.google.com/file/d/122wyJSFMfP61yciEuM2q_JKrYpv2FNFf/view?usp=sharing' style={{textDecoration:"none"}} download><Button  _hover={{ bg: 'gray.700' }} style={{fontSize:"20px" ,width:"250px"}} variant='outline' justifyContent={"space-evenly"} color={"white"}><Icon as={RiFileUserLine} w={6} h={6}/>My Resume</Button>
+                    <a href='https://drive.google.com/file/d/122wyJSFMfP61yciEuM2q_JKrYpv2FNFf/view?usp=sharing' style={{textDecoration:"none"}} target={'_blank'}><Button  _hover={{ bg: 'gray.700' }} style={{fontSize:"20px" ,width:"250px"}} variant='outline' justifyContent={"space-evenly"} color={"white"} onClick={onButtonClick}><Icon as={RiFileUserLine} w={6} h={6}/>My Resume</Button>
                     </a>
                     <div className="headicons"></div>
                     <br />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,12 @@
 
 import React from 'react'
 import { HashLink } from "react-router-hash-link";
-import  {RiFileUserLine}  from 'react-icons/ri'
-import { Flex, Spacer,Button,Heading,ButtonGroup,Box, Icon } from '@chakra-ui/react'
+import { RiFileUserLine } from 'react-icons/ri'
+import { Flex, Spacer, Button, Heading, ButtonGroup, Box, Icon } from '@chakra-ui/react'
 import "./ccss/Navbar.css"
-import {Menu,MenuButton,MenuList,MenuItem} from '@chakra-ui/react'
-import {HamburgerIcon} from "@chakra-ui/icons"
-import Sammyak_MERN_Resume1 from "../Resume/Sammyak_Deosale_Resume.pdf"
+import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
+import { HamburgerIcon } from "@chakra-ui/icons"
+import Sammyak_MERN_Resume1 from "../Resume/fw19_1004_sammyak_deosale_Resume.pdf"
 
 const links = [
   // { path: "#home", title: "Home" },
@@ -17,56 +17,68 @@ const links = [
 ];
 
 function Navbar() {
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch(Sammyak_MERN_Resume1).then(response => {
+      response.blob().then(blob => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement('a');
+        alink.href = fileURL;
+        alink.download = Sammyak_MERN_Resume1;
+        alink.click();
+      })
+    })
+  }
   return (
+    <Box className="navbarbox">
+      <Box  >
+        <Heading className='logo' color={"white"} fontFamily={'sans-serif'} size='auto'>{"Sammyak"}</Heading>
+      </Box>
+      <Spacer />
+      <Box className='navlink' >
+        <a className="navresume" target={'_blank'} href='https://drive.google.com/file/d/122wyJSFMfP61yciEuM2q_JKrYpv2FNFf/view?usp=sharing' ><Button variant='outline' _hover={{ bg: 'gray.700' }} justifyContent={"space-evenly"} color={"white"} onClick={onButtonClick}><Icon as={RiFileUserLine} />My Resume</Button>
+        </a>
+        {links.map((link) => (
 
+          <HashLink
+            // style={({ isActive }) => {
+            //   return isActive ? activeStyle : defaultStyle;
+            // }}
+            className="active"
+            key={link.path}
+            smooth to={link.path}
+            end
+          ><div className='link'>
 
-<Box className="navbarbox">
-  <Box  >
-    <Heading  className='logo' color={"white"} fontFamily={'sans-serif'} size='auto'>{"Sammyak"}</Heading>
-  </Box>
-  <Spacer />
-  <Box className='navlink' >
-  <a className="navresume" href='https://drive.google.com/file/d/122wyJSFMfP61yciEuM2q_JKrYpv2FNFf/view?usp=sharing' download><Button  variant='outline' _hover={{ bg: 'gray.700' }} justifyContent={"space-evenly"} color={"white"}><Icon as={RiFileUserLine} />My Resume</Button>
-    </a>
-  {links.map((link) => (
-        
-        <HashLink
-          // style={({ isActive }) => {
-          //   return isActive ? activeStyle : defaultStyle;
-          // }}
-          className="active"
-          key={link.path}
-         smooth to={link.path}
-          end
-        ><div className='link'>
-          
-          {link.title}</div>
-        </HashLink>
-      ))}
-  </Box>
- 
-  <Box className="menubutton">
-  <Menu >
-  <MenuButton height="70%"  width="9%" padding={"3"} color="white" _hover={{ bg: 'gray.700' }}  as={Button} backgroundColor="#172b4d">
-  <HamburgerIcon height="80%"  width="80%" />
-  </MenuButton>
+              {link.title}</div>
+          </HashLink>
+        ))}
+      </Box>
 
-  <MenuList  className="menu" >
-  {links.map((link) => (
-      <HashLink 
-      key={link.path}
-     smooth to={link.path}
-      end>
-  <MenuItem className="menuItem"  ><p>{link.title}</p></MenuItem>
-  </HashLink>
-  )
-  )
-}
-  </MenuList>
-</Menu>
-</Box>
-   
-</Box>
+      <Box className="menubutton">
+        <Menu >
+          <MenuButton height="70%" width="9%" padding={"3"} color="white" _hover={{ bg: 'gray.700' }} as={Button} backgroundColor="#172b4d">
+            <HamburgerIcon height="80%" width="80%" />
+          </MenuButton>
+
+          <MenuList className="menu" >
+            {links.map((link) => (
+              <HashLink
+                key={link.path}
+                smooth to={link.path}
+                end>
+                <MenuItem className="menuItem"  ><p>{link.title}</p></MenuItem>
+              </HashLink>
+            )
+            )
+            }
+          </MenuList>
+        </Menu>
+      </Box>
+
+    </Box>
 
 
   )
